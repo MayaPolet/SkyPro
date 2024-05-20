ac3ef56015ad2c44892da841828bef54fd61dea7
Maya Poletaeva <mfp.ru>
2024-05-20 17:42:13 +0300

    в main строка для проверки PR

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
 # mask_info Принимать на вход строку с информацией — тип карты/счета и номер карты/счета.
 # Возвращать исходную строку с замаскированным номером карты/счета.
 #
-from masks import mask_account, mask_card
+from src.masks import mask_account, mask_card
 
 # import masks.mask_account
 # import masks.mask_card
